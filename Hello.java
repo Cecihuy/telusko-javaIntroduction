0bb6040aeb42fb1948d8d92ec682b71a8fbb69e1
sourceTree <sourcetree@mail.com>
2023-12-28 12:04:12 -0700

    out of range type conversion will perform modulo(%)

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 /**
- * int to byte (large to smaller type) conversion is not allowed
+ * full range of byte is 256. if conversion is larger than range,
+ * it will perform modulo(%) by 256
  */
 class Hello{
     public static void main(String[] args){        
-        int a = 12;
-        byte b = (byte) a;  //explicit type convertion needed
+        int a = 259;
+        byte b = (byte) a;  //will perform mod 256
 
         System.out.println(b);
     }
