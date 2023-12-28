255227c7f80ae9121befea95ea825de68af17599
sourceTree <sourcetree@mail.com>
2023-12-28 12:35:37 -0700

    #9 Assignment Operators

--- Hello.java
+++ Hello.java
@@ -1,12 +1,12 @@
-/**
- * byte out of range result will be promote into higher type (int)
- */
 class Hello{
     public static void main(String[] args){        
-        byte a = 10;
-        byte b = 30;
-        int result = a * b;     //result is 300 which is byte can't hold
+        int num = 7;
+/* uncomment one of these statement. the result is same */
+//        num = num + 1;
+//        num += 1;
+//        num++;
+//        ++num;
 
-        System.out.println(result);
+        System.out.println(num);
     }
 }
