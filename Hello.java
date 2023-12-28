f324876a7e3dfdb399a15f38323aae33169832ce
sourceTree <sourcetree@mail.com>
2023-12-28 11:28:22 -0700

    #8 Type Conversion

--- Hello.java
+++ Hello.java
@@ -1,15 +1,8 @@
 class Hello{
     public static void main(String[] args){
-        int num = 2_147_483_647;    //decimal. underscore for reading purpose
-        int num2 = 0b101;           //binary
-        int num3 = 0x7E;            //hexa
-        double db = 56;
-        double db2 = 12E10;         //exponen
+        byte b = 125;
+        int a = b;
 
-        System.out.println(num);
-        System.out.println(num2);
-        System.out.println(num3);
-        System.out.println(db);
-        System.out.println(db2);
+        System.out.println(a);
     }
 }
