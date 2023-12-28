e578b08025c4b7f6e599e3a422108e0f55ea0951
sourceTree <sourcetree@mail.com>
2023-12-28 12:47:43 -0700

    pre and post increment difference

--- Hello.java
+++ Hello.java
@@ -1,12 +1,14 @@
+/**
+ * post and pre increment result will be different in fetch perform
+ */
 class Hello{
     public static void main(String[] args){        
         int num = 7;
-/* uncomment one of these statement. the result is same */
-//        num = num + 1;
-//        num += 1;
-//        num++;
-//        ++num;
+        int result = 0;
+/* uncomment one of these statement */
+//        result = num++;     //fetch the value and then increment. result=7
+//        result = ++num;     //increment and then fetch the value. result=8
 
-        System.out.println(num);
+        System.out.println(result);
     }
 }
