3ba04ac0e29127cb8ba21f5cd6d15954ff4b1aa1
sourceTree <sourcetree@mail.com>
2023-12-29 09:40:45 -0700

    #30 jagged and 3D Array

--- Hello.java
+++ Hello.java
@@ -1,29 +1,21 @@
 /**
- * multidimentional array instanciate describe like a table.
- * first bracket is rows(number of arrays)
- * second bracket is columns(number of element each arrays)
+ * jagged array is similar to multidimentional
+ * but column bracket(elements) is dynamic
  */
 class Hello{
     public static void main(String[] args){
-        int nums[][] = new int[3][4];
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 4; j++){
-                System.out.print(nums[i][j] + " ");
-            }
-            System.out.println();
-        }
+        int nums[][] = new int[3][];
+        nums[0] = new int[3];
+        nums[1] = new int[4];
+        nums[2] = new int[2];
 
-        System.out.println();
         //inserting random value        
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 4; j++){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums[i].length; j++){
                 nums[i][j] = (int)(Math.random()*10);
-                System.out.print(nums[i][j] + " ");
             }
-            System.out.println();
         }
-
-        System.out.println();
+        
         //enhance forLoop
         for(int n[]:nums){
             for(int m:n){
