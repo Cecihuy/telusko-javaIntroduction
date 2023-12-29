9bbc77d4c7472d8f1e44342180170ac432c821df
sourceTree <sourcetree@mail.com>
2023-12-28 21:50:32 -0700

    #18 Do While Loop

--- Hello.java
+++ Hello.java
@@ -1,18 +1,12 @@
 /**
- * try to print "Hi" 4 times and
- * each "Hi" print "Hello" 2 times
+ * try to print "Hi" 4 times
  */
 class Hello{
     public static void main(String[] args){
         int n = 1;
-        while(n <= 4){
+        do{
             System.out.println("Hi");
-            int m = 1;
-            while(m <= 2){
-                System.out.println("Hello");
-                m++;
-            }
             n++;
-        }
+        }while(n <= 4);
     }
 }
