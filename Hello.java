3e79a5c2b5e17037b647bbd2a2de2d76cba59b07
sourceTree <sourcetree@mail.com>
2023-12-28 22:17:04 -0700

    nested for loop

--- Hello.java
+++ Hello.java
@@ -1,10 +1,14 @@
 /**
- * try to print "Hi" 4 times
+ * try to print "Hi" 4 times and
+ * each "Hi" print "Hello" 2 times
  */
 class Hello{
     public static void main(String[] args){
         for(int i = 0; i < 4; i++){
             System.out.println("Hi");
+            for(int j = 0; j < 2; j++){
+                System.out.println("Hello");
+            }
         }            
     }
 }
