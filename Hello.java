f2f5d817787a23924ca35a33609f1331c7b513cd
sourceTree <sourcetree@mail.com>
2023-12-29 00:33:02 -0700

    #29 Multi Dimensional Array

--- Hello.java
+++ Hello.java
@@ -1,23 +1,39 @@
 /**
- * sample array overview
+ * multidimentional array instanciate describe like a table.
+ * first bracket is rows(number of arrays)
+ * second bracket is columns(number of element each arrays)
  */
 class Hello{
     public static void main(String[] args){
-        int nums[] = {5,7,2};        
-        System.out.println(nums[1]);
-
-        nums[1] = 6;    //to change element
-        System.out.println(nums[1]);
-
-        int number[] = new int[4];
-        number[0] = 4;
-        number[1] = 8;
-        number[2] = 3;
-        number[3] = 9;
-        System.out.println(number[1]);
-
-        for(int i = 0; i < 4; i++){
-            System.out.print(number[i] + " ");
+        int nums[][] = new int[3][4];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
         }
+
+        System.out.println();
+        //inserting random value        
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                nums[i][j] = (int)(Math.random()*10);
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        //enhance forLoop
+        for(int n[]:nums){
+            for(int m:n){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        //get specific element
+        System.out.println(nums[1][2]);
     }
 }
