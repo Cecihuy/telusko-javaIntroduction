79cd6a838c8865c0693e4bda85c5259b08c4c52b
sourceTree <sourcetree@mail.com>
2023-12-29 00:09:50 -0700

    #28 Creation of Array

--- Hello.java
+++ Hello.java
@@ -1,20 +1,23 @@
 /**
- * sample method overloading overview
+ * sample array overview
  */
-class Calculator{    
-    public int add(int n1, int n2, int n3){
-        return n1 + n2 + n3;
-    }    
-    public int add(int n1, int n2){
-        return n1 + n2;
-    }
-}
-
 class Hello{
     public static void main(String[] args){
-        Calculator obj = new Calculator();
-        int result = obj.add(3,4);      //can input two or three arguments
-        
-        System.out.println(result);
+        int nums[] = {5,7,2};        
+        System.out.println(nums[1]);
+
+        nums[1] = 6;    //to change element
+        System.out.println(nums[1]);
+
+        int number[] = new int[4];
+        number[0] = 4;
+        number[1] = 8;
+        number[2] = 3;
+        number[3] = 9;
+        System.out.println(number[1]);
+
+        for(int i = 0; i < 4; i++){
+            System.out.print(number[i] + " ");
+        }
     }
 }
