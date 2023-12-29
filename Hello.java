13f77b5bd6c6115b4ab5b6c785935475bc296053
sourceTree <sourcetree@mail.com>
2023-12-28 23:19:07 -0700

    #24 Methods

--- Hello.java
+++ Hello.java
@@ -1,24 +1,23 @@
 /**
- * sample class overview
+ * sample method overview
  */
-class Calculator{
-    int a;
-    
-    public int add(int n1, int n2){
-        int r = n1 + n2;
-        return r;
+class Calculator{    
+    public void playMusic(){
+        System.out.println("Music Playing...");
+    }
+    public String getMeAPen(int cost){
+        if(cost >= 10)
+            return "Pen";
+        return "Nothing";
     }
 }
 
 class Hello{
     public static void main(String[] args){
-        int num1 = 4;
-        int num2 = 5;
+        Calculator obj = new Calculator();
+        obj.playMusic();
+        String str = obj.getMeAPen(10);
 
-        Calculator calc = new Calculator();
-
-        int result = calc.add(num1, num2);
-
-        System.out.println(result);
+        System.out.println(str);
     }
 }
