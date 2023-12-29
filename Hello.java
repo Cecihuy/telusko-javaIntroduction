1b82e8353e34cbec479872e176844d4525567adc
sourceTree <sourcetree@mail.com>
2023-12-28 23:30:44 -0700

    #25 Method Overloading

--- Hello.java
+++ Hello.java
@@ -1,23 +1,20 @@
 /**
- * sample method overview
+ * sample method overloading overview
  */
 class Calculator{    
-    public void playMusic(){
-        System.out.println("Music Playing...");
-    }
-    public String getMeAPen(int cost){
-        if(cost >= 10)
-            return "Pen";
-        return "Nothing";
+    public int add(int n1, int n2, int n3){
+        return n1 + n2 + n3;
+    }    
+    public int add(int n1, int n2){
+        return n1 + n2;
     }
 }
 
 class Hello{
     public static void main(String[] args){
         Calculator obj = new Calculator();
-        obj.playMusic();
-        String str = obj.getMeAPen(10);
-
-        System.out.println(str);
+        int result = obj.add(3,4);      //can input two or three arguments
+        
+        System.out.println(result);
     }
 }
