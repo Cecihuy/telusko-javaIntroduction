bf10bc4622234e326ac99cbc4c72429bbdc5c6c4
sourceTree <sourcetree@mail.com>
2023-12-28 19:28:10 -0700

    #11 Logical Operators

--- Hello.java
+++ Hello.java
@@ -1,18 +1,16 @@
 /**
- * try different relational operators
+ * try different logical operators
  */
 class Hello{
     public static void main(String[] args){        
-        int x = 6;
+        int x = 7;
         int y = 5;
+        int a = 5;
+        int b = 9;
         boolean result = true;
 /* uncomment one of these statement */
-//        result = x < y;
-//        result = x > y;
-//        result = x <= y;
-//        result = x >= y;
-//        result = x == y;
-//        result = x != y;
+//        result = x < y && a < b;
+//        result = x > y || a > b;
 
         System.out.println(result);
     }
