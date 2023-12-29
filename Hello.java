8b97133276b4b30aba5f1057acc9ea83fa55b195
sourceTree <sourcetree@mail.com>
2023-12-28 21:44:30 -0700

    nested while loop

--- Hello.java
+++ Hello.java
@@ -1,11 +1,17 @@
 /**
- * try to print "Hi" 4 times
+ * try to print "Hi" 4 times and
+ * each "Hi" print "Hello" 2 times
  */
 class Hello{
     public static void main(String[] args){
         int n = 1;
         while(n <= 4){
             System.out.println("Hi");
+            int m = 1;
+            while(m <= 2){
+                System.out.println("Hello");
+                m++;
+            }
             n++;
         }
     }
