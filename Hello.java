80acc1bb7f5b2fab03f0066014649641bf46466f
sourceTree <sourcetree@mail.com>
2023-12-29 19:48:25 -0700

    #36 StringBuffer and StringBuilder

--- Hello.java
+++ Hello.java
@@ -1,15 +1,16 @@
 /**
- * sample String object overview
+ * sample of StringBuffer overview
  */
 class Hello{
     public static void main(String[] args){
-        String name = "navin";
-        name = name + " reddy";
-        System.out.println("hello " + name);
-
-        String s1 = "Navin";
-        String s2 = "Navin";
-        
-        System.out.println(s1 == s2);
+        StringBuffer stringBuffer = new StringBuffer("Navin");
+        System.out.println(stringBuffer.capacity());
+        System.out.println(stringBuffer.length());
+        System.out.println(stringBuffer.append(" Reddy"));
+        System.out.println(stringBuffer.deleteCharAt(2));
+        System.out.println(stringBuffer.insert(5, "Java "));
+        stringBuffer.setLength(20);
+        String str = stringBuffer.toString();
+        System.out.println(str);
     }
 }
