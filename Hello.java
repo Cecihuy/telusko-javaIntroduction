c9a952b41014c1eee4dbecbe2dac0d51def9fa34
sourceTree <sourcetree@mail.com>
2023-12-29 22:08:38 -0700

    #42 This keyword

--- Hello.java
+++ Hello.java
@@ -1,29 +1,23 @@
 /**
- * encapsulation overview
+ * implement method setAge without 'this' keyword
+ * 'this' keyword represent the current object
  */
 class Calculator{    
     private int age;
-    private String name;
 
     public int getAge() {
         return age;
     }
-    public void setAge(int age) {
-        this.age = age;
+    public void setAge(int age, Calculator obj) {
+        Calculator obj1 = obj;
+        obj1.age = age;
     }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }    
 }
 class Hello{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Calculator obj = new Calculator();
-        obj.setAge(11);
-        obj.setName("Navin");
+        obj.setAge(11, obj);
 
-        System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj.getAge());
     }
 }
