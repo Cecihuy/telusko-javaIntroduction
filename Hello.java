73ad7fef7ab85a0f413cfda6fc113332759a19dc
sourceTree <sourcetree@mail.com>
2023-12-29 22:19:26 -0700

    #43 Constructor

--- Hello.java
+++ Hello.java
@@ -1,23 +1,38 @@
 /**
- * implement method setAge without 'this' keyword
- * 'this' keyword represent the current object
+ * constructor overview
  */
 class Calculator{    
     private int age;
+    private String name;
+
+    public Calculator(){
+        age = 30;
+        name = "Reddy";
+    }
 
     public int getAge() {
         return age;
     }
-    public void setAge(int age, Calculator obj) {
-        Calculator obj1 = obj;
-        obj1.age = age;
+    public void setAge(int age) {
+        this.age = age;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
 }
 class Hello{
     public static void main(String[] args) {
         Calculator obj = new Calculator();
-        obj.setAge(11, obj);
+        System.out.println(obj.getName() + " : " + obj.getAge());
+        
+        obj.setAge(11);
+        obj.setName("Navin");
 
-        System.out.println(obj.getAge());
+        // System.out.println(obj.getName() + " : " + obj.getAge());
     }
 }
