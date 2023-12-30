8e2165ed162acebf4a7a44dc124c281b61641f37
sourceTree <sourcetree@mail.com>
2023-12-30 02:47:12 -0700

    #49 What is Inheritance

--- Hello.java
+++ Hello.java
@@ -1,18 +1,30 @@
 /**
- * anonymous object use only once
- * we can not reuse anonymous object
+ * inheritance overview
  */
 class Calculator{
-    public Calculator(){
-        System.out.println("object creation proved");
+    public int add(int n1, int n2){       
+        return n1 + n2;
     }
-    public void show(){       
-        System.out.println("in Calculator show method");
-    }   
+    public int sub(int n1, int n2){       
+        return n1 - n2;
+    }
+}
+class Computer extends Calculator{
+    public int multi(int n1, int n2){       
+        return n1 * n2;
+    }
+    public int div(int n1, int n2){       
+        return n1 / n2;
+    }
 }
 class Hello{
     public static void main(String[] args) {
-        new Calculator();           //this anonymous object can't be used
-        new Calculator().show();    //way to use the anonymous object. but only once
+        Computer obj = new Computer();
+        int r1 = obj.add(4, 5);
+        int r2 = obj.sub(7, 3);
+        int r3 = obj.multi(5, 3);
+        int r4 = obj.div(15, 4);
+
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " ");
     }
 }
