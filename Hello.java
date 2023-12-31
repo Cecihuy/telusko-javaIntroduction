6abe426a22ae82d9e0cfc456e5984e5233f27508
sourceTree <sourcetree@mail.com>
2023-12-31 06:28:19 -0700

    #61 Abstract Keyword

--- Hello.java
+++ Hello.java
@@ -1,20 +1,26 @@
 /**
- * wrapper class overview
+ * abstract keyword overview
  */
+abstract class Calculator{
+    public abstract void fly();
+    public abstract void drive();
+    public void playMusic(){
+        System.out.println("playing music...");
+    }
+}
+class Computer extends Calculator{
+    public void fly() {
+        System.out.println("flying...");
+    }
+    public void drive() {
+        System.out.println("driving...");
+    }    
+}
 class Hello{
     public static void main(String[] args) {
-        int num = 7;
-        Integer num1 = num;     //auto-boxing
-        
-        System.out.println(num1);
-
-        int num2 = num1;        //auto-unboxing
-
-        System.out.println(num2);
-
-        String str = "12";
-        int num3= Integer.parseInt(str);
-
-        System.out.println(num3 * 2);
+        Calculator obj = new Computer();
+        obj.drive();
+        obj.playMusic();
+        obj.fly();
     }
 }
