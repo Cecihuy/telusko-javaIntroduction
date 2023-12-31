507c2e31fa987a57b4f5141b73e31fbd142ac863
sourceTree <sourcetree@mail.com>
2023-12-31 06:42:46 -0700

    #62 Inner Class

--- Hello.java
+++ Hello.java
@@ -1,26 +1,26 @@
 /**
- * abstract keyword overview
+ * inner class overview
  */
-abstract class Calculator{
-    public abstract void fly();
-    public abstract void drive();
-    public void playMusic(){
-        System.out.println("playing music...");
-    }
-}
-class Computer extends Calculator{
-    public void fly() {
-        System.out.println("flying...");
-    }
-    public void drive() {
-        System.out.println("driving...");
+class Calculator{
+    int age;
+
+    public void show(){
+        System.out.println("in show...");
     }    
+    class Computer{
+        String name;
+
+        public void config() {
+            System.out.println("in config...");
+        }
+    }
 }
 class Hello{
     public static void main(String[] args) {
-        Calculator obj = new Computer();
-        obj.drive();
-        obj.playMusic();
-        obj.fly();
+        Calculator obj = new Calculator();
+        obj.show();
+
+        Calculator.Computer obj1 = obj.new Computer();
+        obj1.config();
     }
 }
