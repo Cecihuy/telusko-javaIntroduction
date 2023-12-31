3933cfdd4f54dcd2e6a9967111404cc8c8f36d8e
sourceTree <sourcetree@mail.com>
2023-12-31 07:39:20 -0700

    #65 Need of Interface

--- Hello.java
+++ Hello.java
@@ -1,16 +1,30 @@
 /**
- * abstract and anonymous inner class overview
+ * interface overview
  */
-abstract class Calculator{
-    public abstract void show();
+abstract class Computer{
+    public abstract void code();
+}
+class Desktop extends Computer{
+    public void code(){
+        System.out.println("get desktop...");
+    }
+}
+class Laptop extends Computer{
+    public void code(){
+        System.out.println("get laptop...");
+    }
+}
+class Developer{
+    public void devApp(Computer computer){
+        computer.code();
+    }
 }
 class Hello{
     public static void main(String[] args) {
-        Calculator obj = new Calculator(){
-            public void show(){
-                System.out.println("in new show...");
-            }
-        };
-        obj.show();
+        Computer desktop = new Desktop();
+        Computer laptop = new Laptop();
+
+        Developer navin = new Developer();
+        navin.devApp(laptop);
     }
 }
