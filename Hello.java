c8fd4dc0429dcd03f10fe1fb6ecac06c5aa3799b
sourceTree <sourcetree@mail.com>
2023-12-31 09:05:38 -0700

    #70 Enum Class

--- Hello.java
+++ Hello.java
@@ -1,41 +1,31 @@
 /**
  * enum overview
  */
-enum Calculator{
-    RUNNING, FAILED, PENDING, SUCCESS;
+enum Laptop{
+    MACBOOK(2000), XPS(2200), SURFACE, THINKPAD(1800);
+    
+    private int price;
+
+    private Laptop(){
+        price = 500;
+    }
+    private Laptop(int price) {
+        this.price = price;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
 class Hello{
     public static void main(String[] args) {
-        Calculator obj = Calculator.FAILED;
+        Laptop laptop = Laptop.MACBOOK;
+        System.out.println(laptop + " : " + laptop.getPrice());
 
-        if(obj == Calculator.RUNNING)
-            System.out.println("All good");
-        else if(obj == Calculator.FAILED)
-            System.out.println("Try again");
-        else if(obj == Calculator.PENDING)
-            System.out.println("Please wait");
-        else
-            System.out.println("Done");
-
-        switch(obj){
-            case RUNNING:
-                System.out.println("All good");
-                break;
-            case FAILED:
-                System.out.println("Try again");
-                break;
-            case PENDING:
-                System.out.println("Please wait");
-                break;
-            default:
-                System.out.println("Done");
-        }
-
-        switch(obj){
-            case RUNNING -> System.out.println("All good");
-            case FAILED -> System.out.println("Try again");
-            case PENDING -> System.out.println("Please wait");
-            default -> System.out.println("Done");
+        for(Laptop efl:Laptop.values()){
+            System.out.println(efl + " : " + efl.getPrice());
         }
     }
 }
