04339c23f93657efc8b72553be259a6e694462ea
sourceTree <sourcetree@mail.com>
2023-12-31 11:46:14 -0700

    #74 Lambda Expression with return

--- Hello.java
+++ Hello.java
@@ -3,11 +3,12 @@
  */
 @FunctionalInterface
 interface Calculator{
-    void show(int i);
+    int add(int i, int j);
 }
 class Hello{
     public static void main(String[] args) {
-        Calculator obj = i -> System.out.println("in show " + i);
-        obj.show(5);
+        Calculator obj = (i, j) -> i + j;
+        int result = obj.add(5,4);
+        System.out.println(result);
     }
 }
