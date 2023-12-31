abb0460595e379877dd266b8382e9d37102deacf
sourceTree <sourcetree@mail.com>
2023-12-31 11:28:06 -0700

    functional interface with anonymous inner class

--- Hello.java
+++ Hello.java
@@ -5,15 +5,14 @@
 interface Calculator{
     void show();
 }
-class Computer implements Calculator{
-    @Override
-    public void show() {
-        System.out.println("in show");
-    }
-}
 class Hello{
     public static void main(String[] args) {
-        Calculator obj = new Computer();
+        Calculator obj = new Calculator()           
+        {
+            public void show(){
+                System.out.println("in show");
+            }
+        };
         obj.show();
     }
 }
