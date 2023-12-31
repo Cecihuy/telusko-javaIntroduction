f07f07a075f2177f4cc4aeb49d01315f92959581
sourceTree <sourcetree@mail.com>
2023-12-31 12:28:49 -0700

    #78 Try with Multiple Catch

--- Hello.java
+++ Hello.java
@@ -3,14 +3,25 @@
  */
 class Hello{
     public static void main(String[] args) {
-        int i = 0;
-        int j = 0;
+        int i = 2;
+        int j = 0;        
+        int[] k = new int[5];
+        String l = null;
 
         try{
             j = 18/i;
+            System.out.println(l.length());
+            System.out.println(k[2]);
+            System.out.println(k[5]);
         } 
+        catch(ArithmeticException e){
+            System.out.println("cannot divide by zero");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("stay in your limit");
+        }
         catch(Exception e){
-            System.out.println("something went wrong");
+            System.out.println("something went wrong " + e);
         }
 
         System.out.println(j);
