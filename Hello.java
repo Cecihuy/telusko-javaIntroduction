a2367f0feee08f23cc1cabe523f28aeeee87533d
sourceTree <sourcetree@mail.com>
2023-12-31 14:02:00 -0700

    #84 try with resources

--- Hello.java
+++ Hello.java
@@ -1,13 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  * taking input from user overview
  */
 class Hello{
-    public static void main(String[] args){
-        System.out.println("Enter a number");        
-
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        System.out.println(num);
+    public static void main(String[] args) throws IOException{
+        int num = 0;
+        // BufferedReader br = null;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        }
+        // finally{
+        //     br.close();
+        // }
     }
 }
