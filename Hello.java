c8bc86e3918ffa50bfda4648df21e3520e4fa7fe
sourceTree <sourcetree@mail.com>
2023-12-31 06:53:56 -0700

    #63 Anonymous Inner Class

--- Hello.java
+++ Hello.java
@@ -1,26 +1,18 @@
 /**
- * inner class overview
+ * anonymous inner class overview
  */
 class Calculator{
-    int age;
-
     public void show(){
         System.out.println("in show...");
-    }    
-    static class Computer{
-        String name;
-
-        public void config() {
-            System.out.println("in config...");
-        }
     }
 }
 class Hello{
     public static void main(String[] args) {
-        Calculator obj = new Calculator();
+        Calculator obj = new Calculator(){
+            public void show(){
+                System.out.println("in new show...");
+            }
+        };
         obj.show();
-
-        Calculator.Computer obj1 = new Calculator.Computer();
-        obj1.config();
     }
 }
