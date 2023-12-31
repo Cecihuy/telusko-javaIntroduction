d1fc7f8f46dcec0d983723890b6dd54d5a89dd13
sourceTree <sourcetree@mail.com>
2023-12-31 13:49:18 -0700

    using scanner

--- Hello.java
+++ Hello.java
@@ -1,19 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 /**
  * taking input from user overview
  */
 class Hello{
-    public static void main(String[] args) throws IOException {
-        System.out.println("Enter a number");
+    public static void main(String[] args){
+        System.out.println("Enter a number");        
 
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader bf = new BufferedReader(in);
-
-        int num = Integer.parseInt(bf.readLine());
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
         System.out.println(num);
-
-        bf.close();
     }
 }
