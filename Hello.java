81ee2317ea1e388cde2baa16abbfb905bb6014cb
sourceTree <sourcetree@mail.com>
2023-12-31 12:50:14 -0700

    #80 Exception throw keyword

--- Hello.java
+++ Hello.java
@@ -3,23 +3,18 @@
  */
 class Hello{
     public static void main(String[] args) {
-        int i = 2;
-        int j = 0;        
-        int[] k = new int[5];
-        String l = null;
+        int i = 20;
+        int j = 0;
 
         try{
-            j = 18/i;
-            System.out.println(l.length());
-            System.out.println(k[2]);
-            System.out.println(k[5]);
+            j = 18 / i;
+            if(j == 0)
+                throw new ArithmeticException("i dont want print zero");
         } 
         catch(ArithmeticException e){
-            System.out.println("cannot divide by zero");
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("stay in your limit");
-        }
+            j = 18/1;
+            System.out.println("thats the default output " + e);
+        }        
         catch(Exception e){
             System.out.println("something went wrong " + e);
         }
