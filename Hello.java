81abbed9f377d58f3a6dcf2297d8b8fcb73cd4a4
sourceTree <sourcetree@mail.com>
2024-01-01 10:24:52 -0700

    #102 Sealed Classes in Java _ Java 17 features

--- Hello.java
+++ Hello.java
@@ -1,23 +1,25 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
 /**
- * LVTI local variable type inference overview
+ * sealed class overview
  */
-class Calculator{
+sealed class Calculator extends Thread implements Cloneable permits Computer, Desktop{
+
+}
+final class Computer extends Calculator{
+
+}
+non-sealed class Desktop extends Calculator{
+
+}
+class Developer extends Desktop{
+
+}
+sealed interface X permits Y{
+
+}
+non-sealed interface Y extends X{
 
 }
 class Hello{
-    public static void main(String[] args){
-        int a = 9;
-        var b = 8;
-        String var = "Navin";
-        int c;
-        var d = 10;
-        var nums = new int[10];
-        var obj = new Calculator();
+    public static void main(String[] args){        
     }   
 }
