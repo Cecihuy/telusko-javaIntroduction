e59b822de9c4b8a07dc82a956cdd59a4d08fde3f
sourceTree <sourcetree@mail.com>
2023-12-31 21:22:17 -0700

    #87 Thread Priority and Sleep

--- Hello.java
+++ Hello.java
@@ -6,6 +6,11 @@ class Calculator extends Thread{
     public void run(){
         for(int i = 1; i < 10; i++){
             System.out.println("hi");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {                
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -14,6 +19,11 @@ class Computer extends Thread{
     public void run(){
         for(int i = 1; i < 10; i++){
             System.out.println("hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {                
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -21,8 +31,22 @@ class Hello{
     public static void main(String[] args){
         Calculator obj1 = new Calculator();
         Computer obj2 = new Computer();
+        
+        // System.out.println(obj1.getPriority());
+        // obj1.setPriority(Thread.MAX_PRIORITY);
+        // System.out.println(obj1.getPriority());
 
         obj1.start();
+        try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {                
+                e.printStackTrace();
+            }
         obj2.start();
+        try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {                
+                e.printStackTrace();
+            }
     }
 }
