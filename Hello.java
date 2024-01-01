b2e1ae17168479620366d7b838d17db836eccd80
sourceTree <sourcetree@mail.com>
2023-12-31 21:47:08 -0700

    runnable vs thread and make it lambda

--- Hello.java
+++ Hello.java
@@ -1,36 +1,28 @@
 /**
  * Thread overview
  */
-class Calculator implements Runnable{
-    @Override
-    public void run(){
-        for(int i = 1; i < 10; i++){
-            System.out.println("hi");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {                
-                e.printStackTrace();
-            }
-        }
-    }
-}
-class Computer implements Runnable{
-    @Override
-    public void run(){
-        for(int i = 1; i < 10; i++){
-            System.out.println("hello");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {                
-                e.printStackTrace();
-            }
-        }
-    }
-}
 class Hello{
     public static void main(String[] args){
-        Runnable obj1 = new Calculator();
-        Runnable obj2 = new Computer();
+        Runnable obj1 = () -> {
+            for(int i = 1; i < 10; i++){
+                System.out.println("hi");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {                
+                    e.printStackTrace();
+                }
+            }
+        };
+        Runnable obj2 = () -> {
+            for(int i = 1; i < 10; i++){
+                System.out.println("hello");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {                
+                    e.printStackTrace();
+                }
+            }
+        };
 
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
