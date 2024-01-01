0cc41d4dfb3a9c93c95544c065ffba8455ea4fa8
sourceTree <sourcetree@mail.com>
2023-12-31 21:02:10 -0700

    #86 Multiple Threads

--- Hello.java
+++ Hello.java
@@ -1,19 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 /**
- * taking input from user overview
+ * Thread overview
  */
-class Hello{
-    public static void main(String[] args) throws IOException{
-        int num = 0;
-        // BufferedReader br = null;
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            num = Integer.parseInt(br.readLine());
-            System.out.println(num);
+class Calculator extends Thread{
+    @Override
+    public void run(){
+        for(int i = 1; i < 10; i++){
+            System.out.println("hi");
         }
-        // finally{
-        //     br.close();
-        // }
+    }
+}
+class Computer extends Thread{
+    @Override
+    public void run(){
+        for(int i = 1; i < 10; i++){
+            System.out.println("hello");
+        }
+    }
+}
+class Hello{
+    public static void main(String[] args){
+        Calculator obj1 = new Calculator();
+        Computer obj2 = new Computer();
+
+        obj1.start();
+        obj2.start();
     }
 }
