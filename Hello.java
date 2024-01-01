4fabdad2407c7075e4d15db6be3a691c125b83cf
sourceTree <sourcetree@mail.com>
2024-01-01 07:14:23 -0700

    #94 Map

--- Hello.java
+++ Hello.java
@@ -1,26 +1,25 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * hashset overview
+ * hashmap overview
  */
 class Hello{
     public static void main(String[] args) throws InterruptedException{
-        Set<Integer> nums = new HashSet<Integer>();
-        nums.add(6);
-        nums.add(5);
-        nums.add(8);
-        nums.add(2);
+        Map<String, Integer> students = new HashMap<String, Integer>();
+        students.put("Navin", 56);
+        students.put("Harsh", 23);
+        students.put("Sushil", 67);
+        students.put("Kiran", 92);
+        students.put("Harsh", 45);
 
-        System.out.println("iterator");
-        Iterator<Integer> values = nums.iterator();
-        while(values.hasNext())
-            System.out.println(values.next());
+        System.out.println(students.get("Sushil"));
 
-        System.out.println("for loop");
-        for(Integer n:nums){
-            System.out.println(n);
+        System.out.println(students.keySet());
+
+        for(String name:students.keySet()){
+            System.out.println(name + " : " + students.get(name));
         }
+
     }
 }
