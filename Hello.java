4f3e2a2208e23fe215a0c6a7718651efb68e0ca9
sourceTree <sourcetree@mail.com>
2024-01-01 11:30:51 -0700

    #103 Record Classes in Java _ Java 17 features

--- Hello.java
+++ Hello.java
@@ -1,25 +1,72 @@
 /**
- * sealed class overview
+ * record overview
  */
-sealed class Calculator extends Thread implements Cloneable permits Computer, Desktop{
 
+//removing this entire class with record
+/*
+class Calculator{
+    int id;
+    String name;
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Calculator other = (Calculator) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "Calculator [id=" + id + ", name=" + name + "]";
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Calculator(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
-final class Computer extends Calculator{
+*/
+//removing this entire class with record
 
-}
-non-sealed class Desktop extends Calculator{
+record Calculator(int id, String name){}
 
-}
-class Developer extends Desktop{
-
-}
-sealed interface X permits Y{
-
-}
-non-sealed interface Y extends X{
-
-}
 class Hello{
-    public static void main(String[] args){        
+    public static void main(String[] args){
+        Calculator obj1 = new Calculator(1, "Navin");
+        Calculator obj2 = new Calculator(1, "Navin");
+        
+        System.out.println(obj1);        
+        System.out.println(obj1.equals(obj2));
+        System.out.println(obj1.name());
     }   
 }
