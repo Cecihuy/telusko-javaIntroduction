3cdfb3d4f8a6bbb4a8e6b65e81ebfd1bc45d6b01
sourceTree <sourcetree@mail.com>
2024-01-01 06:39:51 -0700

    #92 ArrayList

--- Hello.java
+++ Hello.java
@@ -1,37 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Thread overview
- * just only add 'synchronized' to prevent other thread
- * executing method at same time
+ * arraylist overview
  */
-class Calculator{
-    int count;
-    public synchronized void increment(){
-        count++;
-    }
-}
 class Hello{
     public static void main(String[] args) throws InterruptedException{
-        Calculator calc = new Calculator();
-        Runnable obj1 = () -> {
-            for(int i = 0; i < 10000; i++){
-                calc.increment();
-            }
-        };
-        Runnable obj2 = () -> {
-            for(int i = 0; i < 10000; i++){
-                calc.increment();
-            }
-        };
+        List<Integer> nums =new ArrayList<Integer>();
+        nums.add(6);
+        nums.add(5);
+        nums.add(8);
+        nums.add(2);
 
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
+        System.out.println(nums.get(2));
 
-        t1.start();
-        t2.start();
+        System.out.println(nums.indexOf(5));
 
-        t1.join();
-        t2.join();
-
-        System.out.println(calc.count);
+        System.out.println("forLoop");
+        for(Integer n:nums){
+            System.out.println(n);
+        }
     }
 }
